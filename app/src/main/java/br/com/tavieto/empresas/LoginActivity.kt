540b6d6782b7e5abf8b6b4d77b5acfa957c4cdca
Otávio Moreira <tavieto@outlook.com>
2021-03-24 02:34:51 -0300

    update colors

--- app/src/main/java/br/com/tavieto/empresas/LoginActivity.kt
+++ app/src/main/java/br/com/tavieto/empresas/LoginActivity.kt
@@ -1,17 +1,15 @@
 package br.com.tavieto.empresas
 
-import android.graphics.Color
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var button : AppCompatButton
-    var colorOption = 0
-
-    val COLOR_button_background = "#57BBBC"
-    val COLOR_button_background_error = "#748383"
+    private var colorOption = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,17 +17,27 @@ class LoginActivity : AppCompatActivity() {
 
         button = findViewById(R.id.btnLogin)
         button.setOnClickListener {
+            homeActivity()
+            //changeButtonColor()
+        }
+    }
 
-            when(colorOption){
-                0 -> {
-                    colorOption = 1
-                    button.setBackgroundColor(Color.parseColor(COLOR_button_background_error))
-                }
-                else ->{
-                    colorOption = 0
-                    button.setBackgroundColor(Color.parseColor(COLOR_button_background))
-                }
+    private fun homeActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun changeButtonColor() {
+        when(colorOption){
+            0 -> {
+                colorOption = 1
+                button.setBackgroundColor(ContextCompat.getColor(this, R.color.background_button_error))
+            }
+            else ->{
+                colorOption = 0
+                button.setBackgroundColor(ContextCompat.getColor(this, R.color.background_button))
             }
         }
     }
+
 }
