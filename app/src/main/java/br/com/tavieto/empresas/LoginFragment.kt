628efff8a5ad9adee6d030775fb30a9a03d055b0
Otávio Moreira <tavieto@outlook.com>
2021-04-16 04:45:49 -0300

    add warning login error

--- app/src/main/java/br/com/tavieto/empresas/LoginFragment.kt
+++ app/src/main/java/br/com/tavieto/empresas/LoginFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.fragment.app.Fragment
@@ -21,7 +22,8 @@ class LoginFragment : Fragment() {
     lateinit var button: AppCompatButton
     lateinit var edtEmail: AppCompatEditText
     lateinit var edtPassword: TextInputLayout
-
+    lateinit var progressBar: ProgressBar
+    lateinit var bcProgressBar: View
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,9 +39,14 @@ class LoginFragment : Fragment() {
         button = view.findViewById(R.id.btnLogin)
         edtEmail = view.findViewById(R.id.edtEmail)
         edtPassword = view.findViewById(R.id.edtPassword)
+        progressBar = view.findViewById(R.id.progressBar)
+        bcProgressBar = view.findViewById(R.id.bcProgressBar)
 
         button.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {
+            loading(true)
+            errorMessage(false)
+
+            CoroutineScope(Dispatchers.Main).launch {
                 val response = CompanyService.newInstance().login(
                     LoginRequest(
                         email = edtEmail.text.toString(),
@@ -59,9 +66,46 @@ class LoginFragment : Fragment() {
                     accessToken = response.headers()["access-token"] ?: "",
                     client = response.headers()["client"] ?: "",
                     uid = response.headers()["uid"] ?: ""
-                ))
+                )
+            )
+            errorMessage(false)
+            loading(false)
+        } else {
+            errorMessage(true)
+            loading(false)
+        }
+
+
+    }
+
+    private fun errorMessage(option: Boolean) {
+        val errorText = if (option) resources.getString(R.string.login_error) else null
+
+        if(option) {
+            edtPassword.isErrorEnabled = true
+            edtPassword.error = errorText
+            edtPassword.errorContentDescription = errorText
+            edtPassword.errorIconDrawable = resources.getDrawable(R.drawable.material_ic_error)
+
+            edtEmail.setCompoundDrawablesWithIntrinsicBounds(R.drawable.icon_email, 0, R.drawable.material_ic_error, 0)
+
+        } else {
+            edtPassword.isErrorEnabled = false
+            edtPassword.error = errorText
+            edtPassword.errorContentDescription = errorText
+            edtPassword.errorIconDrawable = null
+
+            edtEmail.setCompoundDrawablesWithIntrinsicBounds(R.drawable.icon_email, 0, 0, 0)
         }
     }
 
+    private fun loading(option: Boolean) {
+        val visibility = setVisibility(option)
+
+        progressBar.visibility = visibility
+        bcProgressBar.visibility = visibility
+    }
+
+    private fun setVisibility(option: Boolean) = if (option) View.VISIBLE else View.GONE
 
 }
